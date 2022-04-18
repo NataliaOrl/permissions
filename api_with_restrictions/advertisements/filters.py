9f88cc4a30fc3ead29a7db647ed68f8b6f9ff016
Your Name <you@example.com>
2022-04-19 01:10:05 +0300

    first commit

---
+++ api_with_restrictions/advertisements/filters.py
@@ -0,0 +1,14 @@
+from django_filters import rest_framework as filters
+from rest_framework.authtoken.admin import User
+
+from .models import Advertisement
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+    created_at = filters.DateFromToRangeFilter()
+    creator = filters.ModelChoiceFilter(queryset=User.objects.all())
+
+    class Meta:
+        model = Advertisement
+        fields = ['created_at', 'status']
---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,28 @@
+from advertisements.serializers import AdvertisementSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from .permissions import IsAdvOwnerOrAdmin
+from .filters import AdvertisementFilter
+from .models import Advertisement
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
+    permission_classes = [IsAuthenticated(), IsAdvOwnerOrAdmin()]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action == "create":
+            return [IsAuthenticated()]
+        elif self.action in ["update", "partial_update", "delete"]:
+            return [IsAuthenticated(), IsAdvOwnerOrAdmin()]
+        return []
